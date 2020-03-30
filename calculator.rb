4140318d2c8437b9c49185bc3f0820606916e3bb
walid younis <walid.younis.90@gmail.com>
2020-03-30 21:20:09 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,7 +4,6 @@
 second_number = 3
 
 sum = first_number + second_number
-sum = first_number - second_number
-sum = first_number * second_number
-sum = first_number / second_number
- end
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
