0a0d4e4e5e450d34654ac7c2ced11a1b4b7b84de
walid younis <walid.younis.90@gmail.com>
2020-03-30 21:19:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,9 @@
 
   first_number = 1
 second_number = 3
+
 sum = first_number + second_number
 sum = first_number - second_number
 sum = first_number * second_number
 sum = first_number / second_number
+ end
